6f8e3871ed48b03805d3b00e8297858278b11397
Nycto <James@RoundEights.com>
2025-01-27 18:45:56 -0800

    Add test to ensure compile time parsing works

---
+++ tests/t_constParsing.nim
@@ -0,0 +1,9 @@
+import json_schema_import, std/unittest
+
+importJsonSchema "./examples/complex_object/schema.json"
+
+suite "Compile time parsing":
+    test "Parsing data at compile time":
+        const data = slurp("./examples/complex_object/samples/data0.json").parseJson.jsonTo(Complex_object)
+        const expect = slurp("./examples/complex_object/samples/data0.json")
+        check(data.toJson.pretty == expect)
