420a7d0f493f87b9bec74121f7f75977cbb8a5c3
Nycto <James@RoundEights.com>
2025-02-24 18:27:17 -0800

    Rewrite binary converters to support compile time

--- src/json_schema_import/private/bin.nim
+++ src/json_schema_import/private/bin.nim
@@ -1,64 +1,96 @@
 import std/[options, json, tables]
 
-func addBytes[T](target: var string, source: T) =
-    let bytes = cast[array[sizeof(source), char]](source)
-    for byt in bytes:
-        target.add(byt)
+iterator eachByte(source: string, idx: var int, kind: typedesc): (int, byte) =
+    for i in 0..<sizeof(kind):
+        yield (i, source[idx + i].byte)
+    idx += sizeof(kind)
 
-func readBytes[T](source: string, idx: var int, kind: typedesc[T]): T =
-    var buffer: array[sizeof(T), byte]
-    for i in 0..<sizeof(T):
-        buffer[i] = cast[seq[byte]](source)[idx + i]
-    idx += sizeof(T)
-    result = cast[T](buffer)
+proc toBinary*(target: var string, source: bool) =
+    target.add(source.char)
+
+proc fromBinary*(_: typedesc[bool], source: string, idx: var int): bool =
+    for _, byt in eachByte(source, idx, bool):
+        result = bool(byt)
+
+
+proc toBinary*[T: SomeInteger](target: var string, source: T) =
+    for i in countdown(sizeof(T) - 1, 0):
+        let byt = (source shr (i * 8)) and 0xFF
+        target.add(byt.char)
+
+proc fromBinary*[T: SomeInteger](_: typedesc[T], source: string, idx: var int): T =
+    result = 0
+    for i, byt in eachByte(source, idx, T):
+        let position = sizeof(T) - 1 - i
+        result = result or (cast[T](byt) shl (position * 8))
+
+
+proc toBinary*[T: enum](target: var string, source: T) =
+    toBinary(target, source.int16)
+
+proc fromBinary*[T: enum](_: typedesc[T], source: string, idx: var int): T =
+    T(fromBinary(int16, source, idx))
+
 
 proc toBinary*(target: var string, source: string) =
-    target.addBytes(source.len.int32)
+    toBinary(target, source.len.int32)
     target.add(source)
 
 proc fromBinary*(_: typedesc[string], source: string, idx: var int): string =
-    let len = source.readBytes(idx, int32)
+    let len = fromBinary(int32, source, idx)
     result = source[idx .. (idx + len - 1)]
     idx += len
 
+
 proc toBinary*[T](target: var string, source: Option[T]) =
     if source.isSome:
-        target.addBytes(true)
+        toBinary(target, true)
         toBinary(target, unsafeGet(source))
     else:
-        target.addBytes(false)
+        toBinary(target, false)
 
 proc fromBinary*[T](_: typedesc[Option[T]], source: string, idx: var int): Option[T] =
-    if source.readBytes(idx, bool):
+    let isSome = fromBinary(bool, source, idx)
+    if isSome:
         return some(fromBinary(T, source, idx))
 
+
 proc toBinary*[T](target: var string, source: seq[T]) =
-    target.addBytes(source.len.int32)
+    toBinary(target, source.len.int32)
     for item in source:
         toBinary(target, item)
 
 proc fromBinary*[T](_: typedesc[seq[T]], source: string, idx: var int): seq[T] =
-    let len = source.readBytes(idx, int32)
+    let len = fromBinary(int32, source, idx)
     result = newSeqOfCap[T](len)
     for i in 0..<len:
         result.add(fromBinary(T, source, idx))
 
-proc toBinary*(target: var string, source: SomeNumber | SomeOrdinal) =
-    target.addBytes(source)
 
-proc fromBinary*[T : SomeNumber | SomeOrdinal](_: typedesc[T], source: string, idx: var int): T =
-    return source.readBytes(idx, T)
+proc toBinary*(target: var string, source: float32) =
+    toBinary(target, cast[uint32](source))
+
+proc fromBinary*(_: typedesc[float32], source: string, idx: var int): float32 =
+    cast[float32](fromBinary(uint32, source, idx))
+
+
+proc toBinary*(target: var string, source: float64) =
+    toBinary(target, cast[uint64](source))
+
+proc fromBinary*(_: typedesc[float64], source: string, idx: var int): float64 =
+    cast[float64](fromBinary(uint64, source, idx))
+
 
 type SomeTable[K, V] = Table[K, V] | OrderedTable[K, V]
 
 proc toBinary*[K, V](target: var string, source: SomeTable[K, V]) =
-    target.addBytes(source.len.int32)
+    toBinary(target, source.len.int32)
     for key, value in tables.pairs(source):
         toBinary(target, key)
         toBinary(target, value)
 
 proc fromBinary*[K; V; T : SomeTable[K, V]](_: typedesc[T], source: string, idx: var int): T =
-    let size = source.readBytes(idx, int32)
+    let size = fromBinary(int32, source, idx)
 
     when T is OrderedTable:
         result = initOrderedTable[K, V](size)
@@ -69,20 +101,25 @@ proc fromBinary*[K; V; T : SomeTable[K, V]](_: typedesc[T], source: string, idx:
         let key = fromBinary(K, source, idx)
         result[key] = fromBinary(V, source, idx)
 
+
 proc toBinary*(target: var string, source: JsonNode) =
-    target.addBytes(source.kind)
+    toBinary(target, source.kind)
     case source.kind
     of JArray: toBinary(target, source.elems)
     of JInt: toBinary(target, source.getInt.int64)
+    of JFloat: toBinary(target, source.getFloat.float64)
     of JString: toBinary(target, source.getStr)
     of JObject: toBinary(target, source.fields)
-    else: raiseAssert("Can't yet encode json of kind: " & $source.kind)
+    of JNull: discard
+    of JBool: toBinary(target, source.getBool)
 
 proc fromBinary*(_: typedesc[JsonNode], source: string, idx: var int): JsonNode =
-    let kind = source.readBytes(idx, JsonNodeKind)
+    let kind = fromBinary(JsonNodeKind, source, idx)
     case kind
     of JArray: return JsonNode(kind: kind, elems: fromBinary(seq[JsonNode], source, idx))
     of JInt: return newJInt(fromBinary(int64, source, idx))
+    of JFloat: return newJFloat(fromBinary(float64, source, idx))
     of JString: return newJString(fromBinary(string, source, idx))
     of JObject: return JsonNode(kind: kind, fields: fromBinary(OrderedTable[string, JsonNode], source, idx))
-    else: raiseAssert("Can't yet decode json of kind: " & $kind)
+    of JNull: return newJNull()
+    of JBool: return newJBool(fromBinary(bool, source, idx))
---
+++ tests/t_toBinary.nim
@@ -0,0 +1,84 @@
+import std/[unittest, strutils, options, tables, json, macros], json_schema_import/private/bin
+
+proc encode[T](value: T): string =
+    toBinary(result, value)
+
+proc decode(encoded: string, kind: typedesc): kind =
+    var i = 0
+    fromBinary(kind, encoded, i)
+
+type EnumValue = enum
+    a, b, c
+
+suite "toBinary and fromBinary conversions":
+
+    macro checkValues(kind: untyped, values: varargs[typed]) =
+        var tests = newStmtList()
+        for value in values:
+            tests.add quote do:
+                require(encode[`kind`](`kind`(`value`)).decode(`kind`) == `kind`(`value`))
+                const encoded = encode[`kind`](`kind`(`value`))
+                require(encoded.decode(`kind`) == `kind`(`value`))
+        return quote:
+            {.push hint[ConvFromXtoItselfNotNeeded]: off.}
+            test "Convert " & $`kind` & " to binary":
+                `tests`
+            {.pop.}
+
+    checkValues(bool, true, false)
+
+    template uintTest(kind: typedesc[SomeInteger]) =
+        checkValues(kind, 0.kind, 1, 100, high(kind), low(kind))
+
+    uintTest(uint64)
+    uintTest(uint32)
+    uintTest(uint16)
+    uintTest(uint8)
+
+    template intTest(kind: typedesc[SomeInteger]) =
+        checkValues(kind, 0.kind, 1, -1, 100, -100, high(kind), low(kind))
+
+    intTest(int64)
+    intTest(int32)
+    intTest(int16)
+    intTest(int8)
+
+    checkValues(EnumValue, a, b, c)
+    checkValues(string, "", "a", "abc", "a".repeat(1000))
+
+    checkValues(Option[int], none(int), some(0), some(1))
+    checkValues(Option[string], none(string), some(""), some("a"))
+
+    template floatTest(kind: typedesc[SomeFloat]) =
+        checkValues(kind, 0.kind, 3.14, 100.0, -100, high(kind), low(kind))
+
+    floatTest(float64)
+    floatTest(float32)
+
+    checkValues(seq[string], @[], @["a"], @["a", "b", "c"])
+    checkValues(seq[int], @[], @[0], @[0, 1, 2])
+
+    checkValues(
+        Table[string, int],
+        initTable[string, int](),
+        {"a": 1}.toTable,
+        {"a": 1, "b": 2}.toTable,
+    )
+
+    checkValues(
+        Table[string, string],
+        initTable[string, string](),
+        {"a": "_".repeat(100)}.toTable,
+        {"a": "abc", "b": "xyz"}.toTable,
+    )
+
+    checkValues(JsonNode,
+        newJNull(),
+        %true,
+        %false,
+        %1,
+        %3.14,
+        %"a",
+        %[ "a", "b", "c" ],
+        %({"a": 1, "b": 2}.toTable)
+    )
